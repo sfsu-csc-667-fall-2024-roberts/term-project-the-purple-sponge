3521bfb57107dd17a78a286fe21324e60317c798
elvisjianglol <wjiang1010@gmail.com>
2024-11-14 16:26:00 -0800

    Finished adding all migrations for all tables

---
+++ migrations/1731573210344_bingocards-table.ts
@@ -0,0 +1,38 @@
+import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+// Table bingo_cards {
+//   id int [pk, increment]
+//   session_id int [ref: > game_sessions.id] // FK referencing game_session that this card belongs to
+//   user_id int [ref: > users.id] // FK referencing user that owns this card
+//   card_data json // JSON representation of the bingo card
+//   card_marker_locations json
+//   is_bingo boolean // Indicates if bingo has been achieved on this card
+// }
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable("bingo_cards", {
+    id: "id", // shorthand for { type 'serial', primaryKey: true }
+    session_id: {
+      type: "integer",
+      references: "game_sessions",
+    },
+    user_id: {
+      type: "integer",
+      references: "users",
+    },
+    card_data: {
+      type: "json",
+    },
+    card_marker_locations: {
+      type: "json",
+    },
+    is_bingo: {
+      type: "boolean",
+    },
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable("bingo_cards");
+}
---
+++ migrations/1731573217292_callednumbers-table.ts
@@ -0,0 +1,31 @@
+import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+// Table called_numbers {
+//   id int [pk, increment]
+//   session_id int [ref: > game_sessions.id] // FK referencing game_session that the called number belongs to
+//   number int // The called number (1 to N) we can do 1 to 75
+//   called_at timestamp
+// }
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable("called_numbers", {
+    id: "id", // shorthand for { type 'serial', primaryKey: true }
+    session_id: {
+      type: "integer",
+      references: "game_sessions",
+    },
+    number: {
+      type: "integer", // called number 1 to 75
+    },
+    called_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable("called_numbers");
+}
