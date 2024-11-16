1fb4bab8db15f1312f47aa56089e2f4db6247411
elvisjianglol <wjiang1010@gmail.com>
2024-11-15 19:18:51 -0800

    Added: login and register pages that work
    Also added db manifest page for exporting objects from the database and storing related sql queries

---
+++ src/server/db/users/index.ts
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+// import { createHash } from "crypto"; not really needed
+import db from "../connection";
+import { FIND_BY_EMAIL_SQL, REGISTER_SQL } from "./sql";
+
+// types for our convenience
+type User = {
+  id: number;
+  username: string;
+  email: string;
+};
+
+type UserWithPassword = User & {
+  password: string;
+};
+
+// Take in username, email, clearTextPassword
+const register = async (
+  username: string,
+  email: string,
+  clearTextPassword: string
+): Promise<User> => {
+  console.log("Registering user");
+  const password = await bcrypt.hash(clearTextPassword, 10);
+  // const gravatar = createHash("sha256").update(email).digest("hex");
+  return await db.one(REGISTER_SQL, [username, email, password]);
+};
+
+/* Take in email and clearTextPassword
+ *  Searches for matching user by email
+ *  Verifies the clearTextPassword with the hashed version in the database
+ */
+const login = async (email: string, clearTextPassword: string) => {
+  //TODO: errorhandle the case if user is not found and the promise rejects
+  const user = await findByEmail(email);
+
+  // compare user inputed password with hashed password from database
+  const isValid = await bcrypt.compare(clearTextPassword, user.password);
+  if (isValid) {
+    console.log("Successfully logged in!");
+    return user;
+  } else {
+    throw new Error("Invalid credentials provided");
+  }
+};
+
+const findByEmail = async (email: string): Promise<UserWithPassword> => {
+  console.log("Searching user table by email...", email);
+  return await db.one(FIND_BY_EMAIL_SQL, [email]);
+};
+
+export default { register, login, findByEmail };
--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -1,10 +1,41 @@
 import express from "express"; // need to do it the ES6 way to avoid false errors with typescript
 import db from "../db/connection";
+import { Users } from "../db/dbmanifest";
 
 const router = express.Router();
 
-router.get("/createToken", (request, response) => {
-  console.log("auth route called");
+router.get("/login", (request, response) => {
+  console.log("login route called");
+  response.render("auth/login_page", { title: "Login Page" });
+});
+
+router.get("/signup", (request, response) => {
+  console.log("register route called");
+  response.render("auth/signup_page", { title: "Sign Up Page" });
+});
+
+router.post("/register", async (request, response) => {
+  const { username, email, password } = request.body;
+  try {
+    const user = await Users.register(username, email, password);
+    response.json(user);
+    // response.redirect("/lobby");
+  } catch (error) {
+    console.error(error);
+    // response.redirect("/auth/register");
+  }
+});
+
+router.post("/login", async (request, response) => {
+  const { email, password } = request.body;
+  try {
+    const user = await Users.login(email, password);
+    response.json(user);
+    // response.redirect("/lobby");
+  } catch (error) {
+    console.error(error);
+    response.redirect("/auth/login");
+  }
 });
 
 export default router;
