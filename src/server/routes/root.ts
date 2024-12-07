475dd243facec89aa999c0d22c65f27da58f6f38
elvisjianglol <wjiang1010@gmail.com>
2024-12-06 20:35:55 -0800

    Clean up old routes no longer being used

--- src/server/routes/root.ts
+++ src/server/routes/root.ts
@@ -12,16 +12,4 @@ router.get("/", (request, response) => {
   // response.render tells application to find our template named "root"
 });
 
-router.get("/landingpage", (request, response) => {
-  response.render("landing_page");
-});
-
-router.get("/login", (request, response) => {
-  response.render("login_page");
-});
-
-router.get("/signup", (request, response) => {
-  response.render("signup_page");
-});
-
 export default router;
