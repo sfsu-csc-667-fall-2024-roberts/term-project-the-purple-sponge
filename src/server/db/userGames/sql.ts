9c4c9e4de38853c149d6993eb2f4da4c717a8de2
elvisjianglol <wjiang1010@gmail.com>
2024-12-15 20:30:17 -0800

    Fix typo with database column

--- src/server/db/userGames/sql.ts
+++ src/server/db/userGames/sql.ts
@@ -1,10 +1,11 @@
 // Addition and Deletion of an entry representing a player's connection to a game
 export const JOIN_GAME = `
-INSERT INTO lk_users_gamerooms (user_id, gameroom_id)
+INSERT INTO lk_users_gamerooms (user_id, game_room_id)
 VALUES ($1, $2)
-RETURNING id
+RETURNING *;
 `;
 
 export const LEAVE_GAME = `
-DELETE FROM lk_users_gamerooms WHERE user_id=$1 AND  gameroom_id=$2 AND id=$3
+DELETE FROM lk_users_gamerooms WHERE user_id=$1 AND  game_room_id=$2 AND id=$3
+RETURNING *;
 `;
