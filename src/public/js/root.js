3a73410dba572966dc6fd77df5bd245e39db7a2c
elvisjianglol <wjiang1010@gmail.com>
2024-12-18 14:06:17 -0800

    removed unecessary for loop
    Fixes #19 and #15

--- src/public/js/root.js
+++ src/public/js/root.js
@@ -9,11 +9,6 @@ async function displayGames() {
     return;
   }
 
-  if (display.innerHTML.trim() !== "") {
-    display.innerHTML = "";
-  } else {
-    display.innerHTML = "Please log in to see ongoing games :)";
-  }
   try {
     const response = await fetch("/games/getGames", { method: "GET" });
     if (!response.ok) {
@@ -24,47 +19,28 @@ async function displayGames() {
       const gameInfoRow = document.createElement("ul");
       gameInfoRow.classList.add("horizontal-list");
 
-      let id;
-      let room_name;
-      let max_players;
-      let timer_speed;
       console.log("gameInfoRow: ", gameroom);
-      for (const key in gameroom) {
-        // console.log(key, ": ", gameroom[key]);
 
-        switch (key) {
-          case "id": {
-            id = document.createElement("li");
-            const textNode = document.createTextNode(`ID: ${gameroom[key]}`);
-            id.appendChild(textNode);
-            break;
-          }
-          case "room_name": {
-            room_name = document.createElement("li");
-            const textNode = document.createTextNode(
-              `Room name: ${gameroom[key]}`
-            );
-            room_name.appendChild(textNode);
-            break;
-          }
-          case "max_players": {
-            max_players = document.createElement("li");
-            const textNode = document.createTextNode(
-              `Max Players: ${gameroom[key]}`
-            );
-            max_players.appendChild(textNode);
-            break;
-          }
-          case "timer_speed": {
-            timer_speed = document.createElement("li");
-            const textNode = document.createTextNode(
-              `Timer Speed: ${gameroom[key]}`
-            );
-            timer_speed.appendChild(textNode);
-            break;
-          }
-        } // end switch
-      }
+      const room_name = document.createElement("li");
+      const textNodeName = document.createTextNode(` ${gameroom.room_name}`);
+      room_name.appendChild(textNodeName);
+
+      const id = document.createElement("li");
+      const textNodeId = document.createTextNode(`ID: ${gameroom.id}`);
+      id.appendChild(textNodeId);
+
+      const max_players = document.createElement("li");
+      const textNodeMaxPlayers = document.createTextNode(
+        `Max players:  ${gameroom.max_players}`
+      );
+      max_players.appendChild(textNodeMaxPlayers);
+
+      const timer_speed = document.createElement("li");
+      const textNodeTimerSpeed = document.createTextNode(
+        `Timer speed: ${gameroom.timer_speed}`
+      );
+      timer_speed.appendChild(textNodeTimerSpeed);
+
       // build the final row in order
       gameInfoRow.appendChild(id);
       gameInfoRow.appendChild(room_name);
@@ -79,7 +55,7 @@ async function displayGames() {
       const join_button = document.createElement("button");
       join_button.innerHTML = "Join";
       join_button.style = "height:35.51";
-      
+
       join_link.appendChild(join_button);
       gameInfoRow.appendChild(join_link);
 
@@ -90,4 +66,4 @@ async function displayGames() {
   }
 }
 
-document.getElementById
+document.getElementById;
