5648578c4096a3b1b5020e2b685656d671198bd2
elvisjianglol <wjiang1010@gmail.com>
2024-12-17 16:00:57 -0800

    Added a way to display available games on homepage
    Need to be logged in to see this feature

--- src/public/js/root.js
+++ src/public/js/root.js
@@ -9,16 +9,68 @@ async function displayGames() {
     return;
   }
 
+  if (display.innerHTML.trim() !== "") {
+    display.innerHTML = "";
+  } else {
+    display.innerHTML = "Please log in to see ongoing games :)";
+  }
   try {
     const response = await fetch("/games/getGames", { method: "GET" });
     if (!response.ok) {
       throw new Error(`Response ${response.status}`);
     }
     const json = await response.json();
-    // console.log("json: ", json);
     json.forEach((gameroom) => {
-      // console.log("printing each room", gameroom);
-      display.innerText += JSON.stringify(gameroom);
+      const gameInfoRow = document.createElement("ul");
+      gameInfoRow.classList.add("horizontal-list");
+
+      let id;
+      let room_name;
+      let max_players;
+      let timer_speed;
+      console.log("gameInfoRow: ", gameroom);
+      for (const key in gameroom) {
+        // console.log(key, ": ", gameroom[key]);
+
+        switch (key) {
+          case "id": {
+            id = document.createElement("li");
+            const textNode = document.createTextNode(`ID: ${gameroom[key]}`);
+            id.appendChild(textNode);
+            break;
+          }
+          case "room_name": {
+            room_name = document.createElement("li");
+            const textNode = document.createTextNode(
+              `Room name: ${gameroom[key]}`
+            );
+            room_name.appendChild(textNode);
+            break;
+          }
+          case "max_players": {
+            max_players = document.createElement("li");
+            const textNode = document.createTextNode(
+              `Max Players: ${gameroom[key]}`
+            );
+            max_players.appendChild(textNode);
+            break;
+          }
+          case "timer_speed": {
+            timer_speed = document.createElement("li");
+            const textNode = document.createTextNode(
+              `Timer Speed: ${gameroom[key]}`
+            );
+            timer_speed.appendChild(textNode);
+            break;
+          }
+        } // end switch
+      }
+      // build the final row in order
+      gameInfoRow.appendChild(id);
+      gameInfoRow.appendChild(room_name);
+      gameInfoRow.appendChild(timer_speed);
+      gameInfoRow.appendChild(max_players);
+      display.appendChild(gameInfoRow);
     });
   } catch (error) {
     console.error(error);
